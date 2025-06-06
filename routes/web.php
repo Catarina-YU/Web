2542898b678dbcd999a4d0f8ee1103fb535f41ca
Catarina-YU <catarinasouzasilvao@gmail.com>
2025-06-06 11:32:25 -0300

    atividade 5.1 e quase terminando 5.2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::resource('categories', CategoryController::class);
